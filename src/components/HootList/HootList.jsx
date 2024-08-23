00ef79df27397334b3971c174c2a21970f895780
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-23 09:47:55 -0700

    Add rendering of hoot list

--- src/components/HootList/HootList.jsx
+++ src/components/HootList/HootList.jsx
@@ -1,7 +1,22 @@
-const HootList = (props) => {
+import { Link } from 'react-router-dom';
+
+const HootList = ({ hoots }) => {
     return (
         <main>
-            Hoot List
+            {hoots.map(hoot => (
+                <Link key={hoot._id} to={`/hoots/${hoot._id}`}>
+                    <article>
+                        <header>
+                            <h2>{hoot.title}</h2>
+                            <p>
+                                {hoot.author.username} posted on
+                                {new Date(hoot.createdAt).toLocaleDateString()}
+                            </p>
+                        </header>
+                        <p>{hoot.text}</p>
+                    </article>
+                </Link>
+            ))}
         </main>
     );
 };
